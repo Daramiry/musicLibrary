77a8ae0230f7c270dc3f203ad74c6a3eead77b76
E'laishah Ramirez <drz0224@gmail.com>
2022-10-29 14:23:13 -0700

    uncomplete

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+        function SearchBar(props) {
+            // let [searchTerm, setSearchTerm] = useState('')
+
+            return (
+                <form>
+                    <input type="text" placeholder="Search Here"
+                    onChange={
+                        (e) => (e) => props.handleSearch(e, e.target.value)
+                        } />
+
+                    <input type="submit" />
+
+                </form>
+            )
+        }
+
+        export default SearchBar
